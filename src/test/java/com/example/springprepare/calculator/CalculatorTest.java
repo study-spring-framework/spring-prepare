650d7131a13fb1b3b9f035b7cfe98bb9e6ec3b77
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-06-13 12:42:00 +0900

    Test code

---
+++ src/test/java/com/example/springprepare/calculator/CalculatorTest.java
@@ -0,0 +1,47 @@
+package com.example.springprepare.calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+    @Test
+    @DisplayName("더하기 테스트")
+    void test1() {
+        Calculator calculator = new Calculator();
+        Double result = calculator.operate(8, "+", 2);
+        System.out.println("result = " + result);
+
+        Assertions.assertEquals(10, result);
+    }
+
+    @Test
+    @DisplayName("빼기 테스트")
+    void test2() {
+        Calculator calculator = new Calculator();
+        Double result = calculator.operate(3,"-",2);
+        System.out.println("result = " + result);
+
+        Assertions.assertEquals(1,result);
+    }
+
+    @Test
+    @DisplayName("빼기 테스트")
+    void test3() {
+        Calculator calculator = new Calculator();
+        Double result = calculator.operate(3,"*",2);
+        System.out.println("result = " + result);
+
+        Assertions.assertEquals(6,result);
+    }
+
+    @Test
+    @DisplayName("나누기 테스트")
+    void test4() {
+        Calculator calculator = new Calculator();
+        Double result = calculator.operate(8, "/", 2);
+        System.out.println("result = " + result);
+
+        Assertions.assertEquals(4, result);
+    }
+}
